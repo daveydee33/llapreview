54b43aeb6806ca0020a06fcc9da8a2ce9fb7a0ad
Dave Degeatano <daveydee33@users.noreply.github.com>
2021-11-24 18:53:48 +0800

    add font to theme

--- lib/theme.dart
+++ lib/theme.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 var appTheme = ThemeData(
     brightness: Brightness.dark,
+    // fontFamily: GoogleFonts.nunito().fontFamily,
     textTheme: const TextTheme(
         bodyText1: TextStyle(fontSize: 18),
         bodyText2: TextStyle(fontSize: 16),
