5d4caf49ade650587f2da23b3e70886b66fade4f
Dave Degeatano <daveydee33@users.noreply.github.com>
2021-11-24 21:22:17 +0800

    add bottom nav and FontAwesome icons

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:llapreview/shared/bottom_nav.dart';
 
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
@@ -6,9 +7,11 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-            child: ElevatedButton(
-                child: Text('about', style: Theme.of(context).textTheme.button),
-                onPressed: () => Navigator.pushNamed(context, '/about'))));
+      body: Center(
+          child: ElevatedButton(
+              child: Text('about', style: Theme.of(context).textTheme.button),
+              onPressed: () => Navigator.pushNamed(context, '/about'))),
+      bottomNavigationBar: BottomNavBar(),
+    );
   }
 }
