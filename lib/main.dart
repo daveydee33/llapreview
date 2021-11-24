fdb339eb3408b00af9c143bff3862f849a1bab61
Dave Degeatano <daveydee33@users.noreply.github.com>
2021-11-24 14:28:33 +0800

    add some sample code

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(const MyApp());
+}
+
+randomColor() {
+  return Color((Random().nextDouble() * 0xFFFFFF).toInt()).withOpacity(1.0);
 }
 
 class MyApp extends StatelessWidget {
@@ -12,7 +17,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.green,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
@@ -58,6 +63,66 @@ class _MyHomePageState extends State<MyHomePage> {
               '$_counter',
               style: Theme.of(context).textTheme.headline4,
             ),
+
+            // Container
+            Container(
+              margin: EdgeInsets.all(10),
+              padding: EdgeInsets.all(10),
+              width: 100,
+              height: 100,
+              decoration: BoxDecoration(
+                color: Colors.red,
+                border: Border.all(
+                  color: Colors.black,
+                  width: 5,
+                ),
+              ),
+              child: Text('Hello World'),
+            ),
+
+// Col with Expanded, but also checkout Flexible
+            // Col(children: [
+            //   Expanded(
+            //     child: Container(
+            //       color: Colors.red,
+            //       height: 100,
+            //     ),
+            //   ),
+            //   Expanded(
+            //     child: Container(
+            //       color: Colors.green,
+            //       height: 100,
+            //     ),
+            //   ),
+            // ])
+
+            // Stack (icon on top of red square)
+            Stack(
+              children: [
+                Container(
+                  color: Colors.red,
+                  width: 100,
+                  height: 100,
+                ),
+                Icon(Icons.verified)
+              ],
+            ),
+
+            // Navigation to About page
+            ElevatedButton(
+              child: Text('Navigate'),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (_) => AboutScreen(),
+                  ),
+                );
+              },
+            ),
+
+            // ...
+            /// END OF Widget Array
           ],
         ),
       ),
@@ -66,6 +131,19 @@ class _MyHomePageState extends State<MyHomePage> {
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class AboutScreen extends StatelessWidget {
+  const AboutScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('about'),
+      ),
     );
   }
 }
