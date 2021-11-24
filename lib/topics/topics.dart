56424bc430a137258a1f23fd195b39bad998d0d3
Dave Degeatano <daveydee33@users.noreply.github.com>
2021-11-24 17:43:06 +0800

    adding a file for each screen

---
+++ lib/topics/topics.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Topics extends StatelessWidget {
+  const Topics({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
