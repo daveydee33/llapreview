40b63e9cbb4f2dc63ac6b4d954dc820b0223eb2f
Dave Degeatano <daveydee33@users.noreply.github.com>
2021-11-26 23:49:47 +0800

    add some comments for reference later

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -9,12 +9,27 @@ class AuthService {
   Future<void> googleLogin() async {
     try {
       final googleUser = await GoogleSignIn().signIn();
+      print('googleUser');
+      print(googleUser);
+      // googleUser:
+      // GoogleSignInAccount:{displayName: Dave Degeatano, email: dave@slash.co, id: 104623972310985216270, photoUrl: https://lh3.googleusercontent.com/a-/AOh14GgEo5HE6P6lo0WjENziEYJ1rkezj03Qjc6x0_8w=s1337, serverAuthCode: null}
       if (googleUser == null) return;
+
       final googleAuth = await googleUser.authentication;
+      print('googleAuth');
+      print(googleAuth);
+      // googleAuth:
+      // GoogleSignInAuthentication:Instance of 'GoogleSignInTokenData'
+
       final authCredential = GoogleAuthProvider.credential(
         accessToken: googleAuth.accessToken,
         idToken: googleAuth.idToken,
       );
+      print('authCredential');
+      print(authCredential);
+      // authCredential
+      // AuthCredential(providerId: google.com, signInMethod: google.com, token: null, idToken: ``, accessToken: ``)
+
       await FirebaseAuth.instance.signInWithCredential(authCredential);
     } on FirebaseAuthException catch (e) {
       print(e);
