151a5a11315ae40d6faae30960a30be357407ae9
Dave Degeatano <daveydee33@users.noreply.github.com>
2021-11-28 18:23:06 +0800

    add topics list with just index number for placeholder

--- lib/topics/topics.dart
+++ lib/topics/topics.dart
@@ -1,25 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:llapreview/services/firestore.dart';
+import 'package:llapreview/services/models.dart';
 import 'package:llapreview/shared/bottom_nav.dart';
+import 'package:llapreview/shared/loading.dart';
+import 'package:llapreview/shared/error.dart';
 
 class Topics extends StatelessWidget {
   const Topics({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Topics')),
-      body: (Container(
-        child: Column(
-          children: [
-            Text('Topics'),
-            ElevatedButton(
-              onPressed: () => print('Yo'),
-              child: Text("Yo"),
-            )
-          ],
-        ),
-      )),
-      bottomNavigationBar: BottomNavBar(),
+    return FutureBuilder<List<Topic>>(
+      future: FirestoreService().getTopics(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const LoadingScreen();
+        } else if (snapshot.hasError) {
+          return Center(
+            child: ErrorMessage(message: snapshot.error.toString()),
+          );
+        } else if (snapshot.hasData) {
+          var topics = snapshot.data!;
+          return Scaffold(
+            appBar: AppBar(title: Text('Topics')),
+            body: GridView.count(
+              crossAxisCount: 2,
+              children: List.generate(100, (index) {
+                return Center(
+                  child: Text('Item $index'),
+                );
+              }),
+            ),
+            bottomNavigationBar: BottomNavBar(),
+          );
+        } else {
+          return const Text('No topics found. Check database.');
+        }
+      },
     );
   }
 }
