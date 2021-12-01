a63846b03b31380f41ec532925e2b6a21613f31b
Dave Degeatano <daveydee33@users.noreply.github.com>
2021-12-01 20:14:57 +0800

    dynamc screens. page controller for nextPage. start, congrats, and question pages

--- lib/quiz/quiz_state.dart
+++ lib/quiz/quiz_state.dart
@@ -4,6 +4,7 @@ import 'package:llapreview/services/models.dart';
 class QuizState with ChangeNotifier {
   double _progress = 0;
   Option? _selected;
+  final PageController controller = PageController();
 
   double get progress => _progress;
   Option? get selected => _selected;
@@ -16,5 +17,12 @@ class QuizState with ChangeNotifier {
   set selected(Option? newValue) {
     _selected = newValue;
     notifyListeners();
+  }
+
+  void nextPage() async {
+    await controller.nextPage(
+      duration: const Duration(milliseconds: 500),
+      curve: Curves.easeOut,
+    );
   }
 }
