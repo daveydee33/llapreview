c7ae14743c0f905ee7ded4101b86785b0a8f733f
Dave Degeatano <daveydee33@users.noreply.github.com>
2021-12-08 15:41:53 +0800

    add try catch for fetch items

--- lib/services/state.dart
+++ lib/services/state.dart
@@ -22,11 +22,17 @@ class Counter with ChangeNotifier, DiagnosticableTreeMixin {
 
   void fetchItems() async {
     final client = Client();
-    final res = await client
-        .get(Uri.parse('http://localhost:4001/v1/items?limit=10&page=1'));
-    final resItems = json.decode(res.body);
-    items = resItems['results']; // or  // items = resItems;
-    notifyListeners();
+
+    try {
+      final res = await client
+          .get(Uri.parse('http://localhost:4001/v1/items?limit=10&page=1'));
+      final resItems = json.decode(res.body);
+      items = resItems['results']; // or  // items = resItems;
+      notifyListeners();
+    } catch (e) {
+      print('Error fetching Items'); // API server offline, etc.
+      print(e);
+    }
   }
 
   /// Makes `Counter` readable inside the devtools by listing all of its properties
