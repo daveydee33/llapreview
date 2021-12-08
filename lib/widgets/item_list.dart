34379345f4fedf4b7aebc2aa3260e0945954226b
Dave Degeatano <daveydee33@users.noreply.github.com>
2021-12-08 18:34:05 +0800

    play audio file

--- lib/widgets/item_list.dart
+++ lib/widgets/item_list.dart
@@ -1,7 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:llapreview/services/services.dart';
-import '../main.dart';
+import 'package:just_audio/just_audio.dart';
+
+// class PlaySound extends StatefulWidget {
+//   const PlaySound({Key? key}) : super(key: key);
+
+//   @override
+//   _PlaySoundState createState() => _PlaySoundState();
+// }
+
+// class _PlaySoundState extends State<PlaySound> {
+//   @override
+//   Widget build(BuildContext context) {
+//     final player = AudioPlayer();
+//     // var duration = await player.setAsset('audio/A/acara.mp3');
+//     // print('!!!! $duration');
+//     player.play();
+
+//     return Container();
+//   }
+// }
 
 class ItemList extends StatelessWidget {
   const ItemList({Key? key}) : super(key: key);
@@ -34,6 +53,12 @@ class ItemCard extends StatelessWidget {
 
   ItemCard(this.item);
 
+  void playSound(String filename) async {
+    final player = AudioPlayer();
+    await player.setAsset('assets/audio/A/${filename}.mp3');
+    player.play();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -41,11 +66,13 @@ class ItemCard extends StatelessWidget {
       child: Column(
         children: [
           ListTile(
-            leading: const Icon(
+            leading: IconButton(
               // Icons.play_arrow_rounded,
               // Icons.play_circle_outlined,
-              Icons.play_circle_rounded,
-              size: 50,
+              icon: Icon(Icons.play_circle_rounded),
+              tooltip: 'Play sound',
+              iconSize: 50,
+              onPressed: () => playSound('${item['title']}'),
             ),
             title: Text('${item['title']}'),
             subtitle: Text(
