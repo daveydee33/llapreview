7422608331ec5f4cf6dea69d79b720da017f4cd9
Dave Degeatano <daveydee33@users.noreply.github.com>
2022-01-02 21:50:27 +0800

    display a list of items in simple cards only

--- lib/collections/collection.dart
+++ lib/collections/collection.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:llapreview/services/models.dart';
+import 'package:provider/provider.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class CollectionScreen extends StatelessWidget {
   final Collection collection;
@@ -38,8 +40,54 @@ class CollectionScreen extends StatelessWidget {
         //   style: const TextStyle(
         //       height: 2, fontSize: 20, fontWeight: FontWeight.bold),
         // ),
-        // QuizList(topic: topic),
+        ItemList(collection: collection),
       ]),
+    );
+  }
+}
+
+class ItemList extends StatelessWidget {
+  final Collection collection;
+  const ItemList({Key? key, required this.collection}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: collection.item_titles.map(
+        (item) {
+          return Card(
+            shape:
+                const RoundedRectangleBorder(borderRadius: BorderRadius.zero),
+            elevation: 4,
+            margin: const EdgeInsets.all(4),
+            child: InkWell(
+              onTap: () {
+                // Navigator.of(context).push(
+                //   MaterialPageRoute(
+                //     builder: (BuildContext context) =>
+                //         QuizScreen(quizId: quiz.id),
+                //   ),
+                // );
+              },
+              child: Container(
+                padding: const EdgeInsets.all(8),
+                child: ListTile(
+                  title: Text(
+                    item, // item.title,
+                    style: Theme.of(context).textTheme.headline5,
+                  ),
+                  // subtitle: Text(
+                  //   item, // item.description
+                  //   overflow: TextOverflow.fade,
+                  //   style: Theme.of(context).textTheme.subtitle2,
+                  // ),
+                  // leading: QuizBadge(topic: topic, quizId: quiz.id),
+                ),
+              ),
+            ),
+          );
+        },
+      ).toList(),
     );
   }
 }
