8786d3e476b6b0eff0c419bbf8b3eb94b7634282
Dave Degeatano <daveydee33@users.noreply.github.com>
2022-01-20 14:01:42 +0800

    Revert "WIP - add a search bar from tutorial"
    This reverts commit 91dc3734184615c7501f959e542003803d985d37.

--- lib/words/words.dart
+++ lib/words/words.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:provider/provider.dart';
 import 'package:llapreview/services/services.dart';
 import 'package:llapreview/widgets/item_list.dart';
@@ -16,84 +15,8 @@ class WordsScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Words & Phrases'),
         backgroundColor: Colors.orange[800],
-        actions: [
-          IconButton(
-            icon: const Icon(
-              FontAwesomeIcons.search,
-              // color: Colors.white60,
-            ),
-            onPressed: () {
-              showSearch(context: context, delegate: CustomSearchDelegate());
-            },
-          )
-        ],
       ),
       body: ItemList(items: items),
-    );
-  }
-}
-
-class CustomSearchDelegate extends SearchDelegate<String> {
-  List<String> allData = ['aaa', 'bbb'];
-
-  @override
-  List<Widget>? buildActions(BuildContext context) {
-    return [
-      IconButton(
-        onPressed: () {
-          query = '';
-        },
-        icon: const Icon(Icons.clear),
-      ),
-    ];
-  }
-
-  @override
-  Widget? buildLeading(BuildContext context) {
-    return IconButton(
-      onPressed: () {
-        close(
-            context, null); // TODO: because this second is supposed to be null
-      },
-      icon: const Icon(Icons.arrow_back),
-    );
-  }
-
-  @override
-  Widget buildSuggestions(BuildContext context) {
-    List<String> matchQuery = [];
-    for (var item in allData) {
-      if (item.toLowerCase().contains(query.toLowerCase())) {
-        matchQuery.add(item);
-      }
-    }
-    return ListView.builder(
-      itemCount: matchQuery.length,
-      itemBuilder: (context, index) {
-        var result = matchQuery[index];
-        return ListTile(
-          title: Text(result),
-        );
-      },
-    );
-  }
-
-  @override
-  Widget buildResults(BuildContext context) {
-    List<String> matchQuery = [];
-    for (var item in allData) {
-      if (item.toLowerCase().contains(query.toLowerCase())) {
-        matchQuery.add(item);
-      }
-    }
-    return ListView.builder(
-      itemCount: matchQuery.length,
-      itemBuilder: (context, index) {
-        var result = matchQuery[index];
-        return ListTile(
-          title: Text(result),
-        );
-      },
     );
   }
 }
